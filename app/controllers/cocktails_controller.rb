5c9ab2de81cb4003242ec1e1eebed7a215f9c918
Arnaut Van de Perre <arnautvdp@gmail.com>
2019-10-31 16:17:07 +0100

    Rakefile is OK, views need adjusting

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,12 +4,16 @@ class CocktailsController < ApplicationController
   end
 
   def new
-    @cockatil = Cocktail.new
+    @cocktail = Cocktail.new
   end
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def show
